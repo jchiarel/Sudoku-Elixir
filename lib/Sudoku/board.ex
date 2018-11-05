69db007c981a41cae6a96e9528e9d8ed590f83e3
Joe Chiarella <joe.chiarella@salesloft.com>
2018-11-04 22:27:46 -0500

    Added some more board tests with error handling

--- lib/Sudoku/board.ex
+++ lib/Sudoku/board.ex
@@ -1,19 +1,29 @@
 defmodule Sudoku.Board do
   use GenServer
 
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+  def start_link(size) do
+    GenServer.start_link(__MODULE__, [size: size])
   end
 
-  def init(:ok) do
-    {:ok, %{{1,1} => Sudoku.Tile.start_link([])}}
+  def init([size: 4]) do
+    {:ok, %{:size => 4,
+        {1,1} => Sudoku.Tile.start_link([])}}
+  end
+
+  def init(_) do
+    {:error, "Invalid board config"}
   end
 
   def get_tile(server, loc) when is_tuple(loc) do
     GenServer.call(server, {:get_tile, loc})
   end
 
-  def handle_call({:get_tile, loc}, _from, state) do
+  def handle_call({:get_tile, {x, y} = loc}, _from, %{size: size} = state)
+      when x <= size and y <= size and x > 0 and y > 0 do
     {:reply, Map.get(state, loc), state}
+  end
+
+  def handle_call({:get_tile, _}, _, state) do
+    {:reply, {:error, "Invalid tile"}, state}
   end
 end
--- test/Sudoku/board_test.exs
+++ test/Sudoku/board_test.exs
@@ -4,7 +4,7 @@ defmodule Sudoku.BoardTest do
   alias Sudoku.Board
 
   setup do
-    board = start_supervised!(Sudoku.Board)
+    board = start_supervised!({Board, 4})
     %{board: board}
   end
 
@@ -12,5 +12,10 @@ defmodule Sudoku.BoardTest do
     {:ok, tile} = Board.get_tile(board, {1, 1})
     possible = Sudoku.Tile.get_possible(tile)
     assert possible == [1, 2, 3, 4]
+
+    {:error, _} = Board.get_tile(board, {5, 4})
+    {:error, _} = Board.get_tile(board, {4, 5})
+    {:error, _} = Board.get_tile(board, {0, 4})
+    {:error, _} = Board.get_tile(board, {-4, 3})
   end
 end
